518254f0fe6c0d1798a16f086818d1452879389f
norbadboy <norbadboy@gmail.com>
2025-05-29 13:10:51 +0200

    fix: Update import statement for GlobalStyles to include file extension

--- my-portfolio/src/index.jsx
+++ my-portfolio/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles";
+import GlobalStyles from "./styles/GlobalStyles.jsx";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 
