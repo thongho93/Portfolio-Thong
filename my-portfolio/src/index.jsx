ad89c7632b030fb9ff77b6129f04bb9753bff60b
norbadboy <norbadboy@gmail.com>
2025-05-29 13:20:09 +0200

    fix: Remove file extension from GlobalStyle import in index.jsx

--- my-portfolio/src/index.jsx
+++ my-portfolio/src/index.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import GlobalStyle from "./styles/GlobalStyles.jsx";
+import GlobalStyle from "./styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 
@@ -22,7 +21,4 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
